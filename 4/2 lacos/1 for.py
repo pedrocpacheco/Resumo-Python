76668e5cd73c8ba44421faa3286268fc78db2253
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-16 17:17:51 -0300

    loops, for and while

---
+++ 4/2 lacos/1 for.py
@@ -0,0 +1,9 @@
+for numero in range(10):
+    print(f"Numero atual: {numero}")
+# Interando em uma sequenncia de dados
+
+alunos = ["Pedro", "Rafael", "Diogo"]
+
+for aluno in alunos:
+    print(f"Olá, {aluno}")
+# Acontece para cada valor
---
+++ 4/2 lacos/2 while.py
@@ -0,0 +1,6 @@
+numero = 1
+while numero < 10:
+    print(f"Valor atual: {numero}")
+    numero += 1
+    
+# Printa até o 9
