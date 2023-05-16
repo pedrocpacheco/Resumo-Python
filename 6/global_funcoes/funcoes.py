0258562f351ba1eded086f24e746589a4b5b978d
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-16 18:39:00 -0300

    starting global functions

---
+++ 6/global_funcoes/funcoes.py
@@ -0,0 +1,12 @@
+number = -5
+print(abs(number)) # Recebe um numero, e retorna o seu valor absoluto.
+# Resultado -> -5
+
+dic = dict(nome="Pedro", sobrenome="Pacheco", idade=17) # Recebe as chaves e os valores
+print(dic)
+# Resultado -> {'nome': 'Pedro', 'sobrenome': 'Pacheco', 'idade': 17}
+
+lista = [1, 2, 3, 4, 5]
+
+print(dir(lista))
+print(dir)
