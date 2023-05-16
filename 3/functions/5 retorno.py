9bc3d9d2fa568206b5cdd04c38a6c886e8f7ca6a
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-16 16:59:04 -0300

    function's return

---
+++ 3/functions/5 retorno.py
@@ -0,0 +1,8 @@
+def calcular_media(nota_1, nota_2):
+    media = (nota_1 + nota_2) / 2
+    return media
+    # A função acaba no return
+
+media = calcular_media(10, 8.5)
+print(f"Printando media do Aluno Pedro: {media}")
+# Explicação -> A função calcular_media retornou um valor, que foi salvo em média.
